4f97359fe4fe3939b1589c487b427848fef19ef8
apollohpp <apollo.hpp@gmail.com>
2019-05-05 01:59:03 +0900

    Now I'm looking for how to check collision.

--- tetris.ps1
+++ tetris.ps1
@@ -1,24 +1,44 @@
-# Draw
+# Out
+$windowSize = $host.ui.rawui.WindowSize;
 $backGroundColor = $Host.UI.RawUI.BackgroundColor;
 $point = New-Object Management.Automation.Host.Coordinates;
 
-# Game
-$now = @()
-$next = @()
+# Border
+$borderX = 0
+$borderY = 0
+$borderW = 16
+$borderH = 20
 
 # Tetromino
-$minoX = 0
-$minoY = 0
+$minoX = $borderX + ($borderW / 2) - 2
+$minoY = $borderY + 1
 $minoW = 4
 $minoH = 4
 
-# Board
-$boardX = 0
-$boardY = 0
-$boardW = 16
-$boardH = 20
+# Game
+$nowShape = 0
+$nowBuffer = $null
+$nowX = $minoX
+$nowY = $minoY
+$rotation = 0
 
-# Const
+$minoFrame = New-Object Management.Automation.Host.Rectangle;
+$minoFrame.Left = $minoX
+$minoFrame.Top = $minoY
+$minoFrame.Right = $minoX + $minoW
+$minoFrame.Bottom = $minoY + $minoH
+
+$nextBuffer = $null
+$nextX = $borderX + $borderW + 4
+$nextY = 4
+
+$gameFrame = New-Object Management.Automation.Host.Rectangle;
+$gameFrame.Left = 0
+$gameFrame.Top = 0
+$gameFrame.Right = $windowSize.Width
+$gameFrame.Bottom = $windowSize.Height
+
+# Tetromino Shapes
 $I = 0
 $J = 1
 $L = 2
@@ -26,80 +46,82 @@ $O = 3
 $S = 4
 $T = 5
 $Z = 6
+$maxShape = 6
+$maxRotation = 4
 
 <#
-OOOO
-XXXX
-OOOO
-OOOO
+    
+@@@@
+    
+    
 #>
-$I0 = 'OOOOXXXXOOOOOOOO'
-$I1 = 'OOXOOOXOOOXOOOXO'
-$I2 = 'OOOOOOOOXXXXOOOO'
-$I3 = 'OXOOOXOOOXOOOXOO'
+$I0 = @('    ', '@@@@', '    ', '    ')
+$I1 = @('  @ ', '  @ ', '  @ ', '  @ ')
+$I2 = @('    ', '    ', '@@@@', '    ')
+$I3 = @(' @  ', ' @  ', ' @  ', ' @  ')
 
 <#
-XOOO
-XXXO
-OOOO
-OOOO
+@   
+@@@ 
+    
+    
 #>
-$J0 = 'XOOOXXXOOOOOOOOO'
-$J1 = 'OXXOOXOOOXOOOOOO'
-$J2 = 'OOOOXXXOOOXOOOOO'
-$J3 = 'OXOOOXOOXXOOOOOO'
+$J0 = @('@   ', '@@@ ', '    ', '    ')
+$J1 = @(' @@ ', ' @  ', ' @  ', '    ')
+$J2 = @('    ', '@@@ ', '  @ ', '    ')
+$J3 = @(' @  ', ' @  ', '@@  ', '    ')
 
 <#
-OOXO
-XXXO
-OOOO
-OOOO
+  @ 
+@@@ 
+    
+    
 #>
-$L0 = 'OOXOXXXOOOOOOOOO'
-$L1 = 'OXOOOXOOOXXOOOOO'
-$L2 = 'OOOOXXXOXOOOOOOO'
-$L3 = 'XXOOOXOOOXOOOOOO'
+$L0 = @('  @ ', '@@@ ', '    ', '    ')
+$L1 = @(' @  ', ' @  ', ' @@ ', '    ')
+$L2 = @('    ', '@@@ ', '@   ', '    ')
+$L3 = @('@@  ', ' @  ', ' @  ', '    ')
 
 <#
-OXXO
-OXXO
-OOOO
-OOOO
+ @@ 
+ @@ 
+    
+    
 #>
-$O0 = 'OXXOOXXOOOOOOOOO'
+$O0 = @(' @@ ', ' @@ ', '    ', '    ')
 
 <#
-OXXO
-XXOO
-OOOO
-OOOO
+ @@ 
+@@  
+    
+    
 #>
-$S0 = 'OXXOXXOOOOOOOOOO'
-$S1 = 'OXOOOXXOOOXOOOOO'
-$S2 = 'OOOOOXXOXXOOOOOO'
-$S3 = 'XOOOXXOOOXOOOOOO'
+$S0 = @(' @@ ', '@@  ', '    ', '    ')
+$S1 = @(' @  ', ' @@ ', '  @ ', '    ')
+$S2 = @('    ', ' @@ ', '@@  ', '    ')
+$S3 = @('@   ', '@@  ', ' @  ', '    ')
 
 <#
-OXOO
-XXXO
-OOOO
-OOOO
+ @  
+@@@ 
+    
+    
 #>
-$T0 = 'OXOOXXXOOOOOOOOO'
-$T1 = 'OXOOOXXOOXOOOOOO'
-$T2 = 'OOOOXXXOOXOOOOOO'
-$T3 = 'OXOOXXOOOXOOOOOO'
+$T0 = @(' @  ', '@@@ ', '    ', '    ')
+$T1 = @(' @  ', ' @@ ', ' @  ', '    ')
+$T2 = @('    ', '@@@ ', ' @  ', '    ')
+$T3 = @(' @  ', '@@  ', ' @  ', '    ')
 
 <#
-OXXO
-OOXX
-OOOO
-OOOO
+ @@ 
+  @@
+    
+    
 #>
-$Z0 = 'XXOOOXXOOOOOOOOO'
-$Z1 = 'OOXOOXXOOXOOOOOO'
-$Z2 = 'OOOOXXOOOXXOOOOO'
-$Z3 = 'OXOOXXOOXOOOOOOO'
+$Z0 = @('@@  ', ' @@ ', '    ', '    ')
+$Z1 = @('  @ ', ' @@ ', ' @  ', '    ')
+$Z2 = @('    ', '@@  ', ' @@ ', '    ')
+$Z3 = @(' @  ', '@@  ', '@   ', '    ')
 
 $shapes = @(
     ($I0, $I1, $I2, $I3),
@@ -111,56 +133,82 @@ $shapes = @(
     ($Z0, $Z1, $Z2, $Z3)
 )
 
-function NewTetromino($type) {
-    $shape = $script:shapes[$type][0]
+$colors = @(
+    [System.ConsoleColor]::Gray,
+    [System.ConsoleColor]::Green,
+    [System.ConsoleColor]::Cyan,
+    [System.ConsoleColor]::Red,
+    [System.ConsoleColor]::Magenta,
+    [System.ConsoleColor]::Yellow,
+    [System.ConsoleColor]::White
+);
 
-    #Write-Host $shape
-    $mino = @()
-
-    $i = 0
-    for($y = 0; $y -lt $script:minoH; $y++) {
-        for($x = 0; $x -lt $script:minoW; $x++) {
-            if($shape[$i] -eq 'X') {
-                $mino += $true
-            } else {
-                $mino += $false
-            }
-            $i++
-        }
-    }
-
-    return $mino
+function Update-NowTetromino($shape) {
+    $script:nowX = $script:minoX
+    $script:nowY = $script:minoY
+    $script:rotation = 0
+    $script:nowShape = $shape
 }
 
-function DrawBoard() {    
+function Update-NextTetromino($shape) {
+    $mino = $script:shapes[$shape][0]
+    $color = $script:colors[$shape]
+
+    $script:nextBuffer = $Host.UI.RawUI.NewBufferCellArray($mino, $color, $script:backGroundColor)
 }
 
-function DrawNow() {
-    $x = 0
-    $y = 0
-    foreach($c in $script:now) {
-        if($c -eq $true) {
-            $cell = $script:buffer[$y, $x]
-            $cell.Character = "@"
-            $cell.ForegroundColor = [System.ConsoleColor]::Magenta
-            $script:buffer[$y, $x] = $cell
-        }
+function Write-Border() {
+    [String[]]$border = @();
+    
+    $line1 = "|"; 
+    $line2 = "O";
+    1..$borderW | ForEach-Object { $line1 += " "; $line2 += "="; }
+    $line1 += "|"; 
+    $line2 += "O";
 
-        $x++
-        if($x -ge $script:minoW) {
-            $x = 0
-            $y++
-        } 
-    }
+    $border += $line2;
+    1..$borderH | ForEach-Object { $border += $line1; }
+    $border += $line2;
+
+    $buffer = $Host.UI.RawUI.NewBufferCellArray($border, [system.consolecolor]::Green, $script:backGroundColor);
+
+    Write-Buffer $borderX $borderY $buffer
 }
 
-function Draw($x, $y, $buffer) {
+function Write-NextTetromino() {
+    Write-Buffer $script:nextX $script:nextY $script:nextBuffer
+}
+
+function Test-Collision() {
+}
+
+function Update-Rotation() {
+    $script:rotation = ($script:rotation + 1) % $script:maxRotation
+}
+
+function Write-NowTetromino() {
+    $shape = $script:shapes[$script:nowShape][$script:rotation]
+    $color = $script:colors[$script:nowShape]
+
+    $script:nowBuffer = $Host.UI.RawUI.NewBufferCellArray($shape, $color, $script:backGroundColor)
+
+    Write-Buffer $script:nowX $script:nowY $script:nowBuffer
+}
+
+function Write-Buffer($x, $y, $buffer) {
     $script:point.x = $x
     $script:point.y = $y
     $Host.UI.RawUI.SetBufferContents($script:point, $buffer)
 }
 
-function GetEvent() {
+function Write-Text($x, $y, $message) {
+    $script:point.x = $x; 
+    $script:point.y = $y;
+    $Host.UI.RawUI.CursorPosition = $script:point;
+    Write-Host $message;
+}
+
+function Get-GameEvent() {
     $e = 0;
 
     if ($Host.UI.RawUi.KeyAvailable){ 
@@ -181,28 +229,40 @@ function GetEvent() {
     return $e
 }
 
-function main() {    
-    $script:now = NewTetromino($L)
-
+function Start-NewGame() {
     Clear-Host
-    $frame = New-Object Management.Automation.Host.Rectangle;
-    $windowSize = $host.ui.rawui.WindowSize;
-    $frame.Left = 0
-    $frame.Top = 0
-    $frame.Right = $windowSize.Width
-    $frame.Bottom = $windowSize.Height
-    $script:frameBuffer = $Host.UI.RawUI.GetBufferContents($frame)
-    $script:buffer = $frameBuffer.Clone()
+    Write-Border
+
+    $rand = Get-random $script:maxShape
+    Update-NowTetromino $rand
+
+    $rand = Get-random $script:maxShape
+    Update-NextTetromino $rand
+
+    Write-NextTetromino
+}
+
+function main() {    
+    Start-NewGame
 
     do {
-        $code = GetEvent
-    
+        $code = Get-GameEvent
+        
         switch($code) {
+            # right
+            39 { $script:nowX++ }
+            # left
+            37 { $script:nowX-- }
+            # up
+            38 { Update-Rotation }
+            # down
+            40 {}
+            # drop
+            32 {}
             default {}
         }
 
-        DrawNow
-        Draw 0 0 $buffer
+        Write-NowTetromino
 
         Start-Sleep -m 10
     } until($code -eq -1)
